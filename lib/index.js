Zearin/metalsmith

commit 23b5228c72516f174b5c7502c19b3b2e6dafe901

refactor(security): Use `Buffer.from` when possible
This lets developers only have to remember to do one thing: use `Buffer.from` everywhere.

For ancient versions of Node.js, `Buffer` doesn’t offer `.from`, so just fall back to the constructor (which is what would have to happen anyway).

(In other words: no need to have to remember to noise-up the code with conditionals, and no need to read through conditional noise.  )

lib/index.js
@@ -30,6 +30,18 @@ var isObject  = function(o) {return o !== null && typeof o === 'object'}
 var isString  = function(s) {return typeof s === 'string'}
 
 
+/* Use `Buffer.from` whenever possible.
+ * For old Node.js versions, just alias `Buffer.from` to the old constructor 
+ * so nobody needs to remember to add conditionals.
+ *
+ * See:
+ *    - https://nodejs.org/api/buffer.html#buffer_new_buffer_string_encoding
+ *    - https://nodejs.org/api/buffer.html#buffer_class_method_buffer_from_string_encoding
+ */
+if (!Buffer.hasOwnProperty('from')) {
+  Buffer.prototype.from = Buffer.prototype.constructor
+}
+
 /**
  * Export `Metalsmith`.
  */
@@ -297,13 +309,7 @@ Metalsmith.prototype.readFile = unyield(function*(file){
       }
 
       ret = parsed.data
-      // Use `Buffer.from` whenever possible
-      // See:
-      //    - https://nodejs.org/api/buffer.html#buffer_new_buffer_string_encoding
-      //    - https://nodejs.org/api/buffer.html#buffer_class_method_buffer_from_string_encoding
-      ret.contents = (Buffer.hasOwnProperty('from')) 
-        ? Buffer.from(parsed.content) 
-        : new Buffer(parsed.content)
+      ret.contents = Buffer.from(parsed.content) 
     } else {
       ret.contents = buffer
     }

test/index.js
@@ -10,6 +10,20 @@ var path = require('path')
 var rm = require('rimraf').sync
 var fixture = path.resolve.bind(path, __dirname, 'fixtures')
 
+
+/* Use `Buffer.from` whenever possible.
+ * For old Node.js versions, just alias `Buffer.from` to the old constructor 
+ * so nobody needs to remember to add conditionals.
+ *
+ * See:
+ *    - https://nodejs.org/api/buffer.html#buffer_new_buffer_string_encoding
+ *    - https://nodejs.org/api/buffer.html#buffer_class_method_buffer_from_string_encoding
+ */
+if (!Buffer.hasOwnProperty('from')) {
+  Buffer.prototype.from = Buffer.prototype.constructor
+}
+
+
 describe('Metalsmith', function(){
   beforeEach(function(){
     rm('test/tmp')
@@ -237,7 +251,7 @@ describe('Metalsmith', function(){
         assert.deepEqual(files, {
           'index.md': {
             title: 'A Title',
-            contents: new Buffer('body'),
+            contents: Buffer.from('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -256,7 +270,7 @@ describe('Metalsmith', function(){
         assert.deepEqual(files, {
           'dir/index.md': {
             title: 'A Title',
-            contents: new Buffer('body'),
+            contents: Buffer.from('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -274,7 +288,7 @@ describe('Metalsmith', function(){
         assert.deepEqual(files, {
           'index.md': {
             title: 'A Title',
-            contents: new Buffer('body'),
+            contents: Buffer.from('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -290,7 +304,7 @@ describe('Metalsmith', function(){
         if (err) return done(err)
         assert.deepEqual(files, {
           'bin': {
-            contents: new Buffer('echo test'),
+            contents: Buffer.from('echo test'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -338,7 +352,7 @@ describe('Metalsmith', function(){
           'index.md': {
             date: new Date('2013-12-02'),
             title: 'A Title',
-            contents: new Buffer('body'),
+            contents: Buffer.from('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -360,7 +374,7 @@ describe('Metalsmith', function(){
           'index.md': {
             date: new Date('2013-12-02'),
             title: 'A Title',
-            contents: new Buffer('body'),
+            contents: Buffer.from('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -373,7 +387,7 @@ describe('Metalsmith', function(){
   describe('#write', function(){
     it('should write to a destination directory', function(done){
       var m = Metalsmith(fixture('write'))
-      var files = { 'index.md': { contents: new Buffer('body') }}
+      var files = { 'index.md': { contents: Buffer.from('body') }}
       m.write(files, function(err){
         if (err) return done(err)
         equal(fixture('write/build'), fixture('write/expected'))
@@ -383,7 +397,7 @@ describe('Metalsmith', function(){
 
     it('should write to a provided directory', function(done){
       var m = Metalsmith(fixture('write-dir'))
-      var files = { 'index.md': { contents: new Buffer('body') }}
+      var files = { 'index.md': { contents: Buffer.from('body') }}
       var dir = fixture('write-dir/out')
       m.write(files, dir, function(err){
         if (err) return done(err)
@@ -398,7 +412,7 @@ describe('Metalsmith', function(){
       var m = Metalsmith(fixture('write-mode'))
       var files = {
         'bin': {
-          contents: new Buffer('echo test'),
+          contents: Buffer.from('echo test'),
           mode: '0777'
         }
       }
@@ -494,7 +508,7 @@ describe('Metalsmith', function(){
         .use(function(files, metalsmith, done){
           Object.keys(files).forEach(function(file){
             var data = files[file]
-            data.contents = new Buffer(data.title)
+            data.contents = Buffer.from(data.title)
           })
           done()
         })
@@ -539,7 +553,7 @@ describe('Metalsmith', function(){
         .use(function(files, metalsmith, done){
           Object.keys(files).forEach(function(file){
             var data = files[file]
-            data.contents = new Buffer(data.title)
+            data.contents = Buffer.from(data.title)
           })
           done()
         })

commit 12f8cf47d690128cbebb8f99e0076d9c6bb3de38

fix: (attempted) Fix for regression in 23b5228c

lib/index.js
@@ -30,17 +30,7 @@ var isObject  = function(o) {return o !== null && typeof o === 'object'}
 var isString  = function(s) {return typeof s === 'string'}
 
 
-/* Use `Buffer.from` whenever possible.
- * For old Node.js versions, just alias `Buffer.from` to the old constructor 
- * so nobody needs to remember to add conditionals.
- *
- * See:
- *    - https://nodejs.org/api/buffer.html#buffer_new_buffer_string_encoding
- *    - https://nodejs.org/api/buffer.html#buffer_class_method_buffer_from_string_encoding
- */
-if (!Buffer.hasOwnProperty('from')) {
-  Buffer.prototype.from = Buffer.prototype.constructor
-}
+
 
 /**
  * Export `Metalsmith`.
@@ -307,9 +297,16 @@ Metalsmith.prototype.readFile = unyield(function*(file){
         err.code = 'invalid_frontmatter'
         throw err
       }
-
+      // Use `Buffer.from` if possible.
+      //
+      // See:
+      //    - https://nodejs.org/api/buffer.html#buffer_new_buffer_string_encoding
+      //    - https://nodejs.org/api/buffer.html#buffer_class_method_buffer_from_string_encoding
+      //
       ret = parsed.data
-      ret.contents = Buffer.from(parsed.content) 
+      ret.contents = (Buffer.hasOwnProperty('from'))
+        ? Buffer.from(parsed.content) 
+        : new Buffer(parsed.content)
     } else {
       ret.contents = buffer
     }

test/index.js
@@ -12,17 +12,16 @@ var fixture = path.resolve.bind(path, __dirname, 'fixtures')
 
 
 /* Use `Buffer.from` whenever possible.
- * For old Node.js versions, just alias `Buffer.from` to the old constructor 
- * so nobody needs to remember to add conditionals.
- *
  * See:
  *    - https://nodejs.org/api/buffer.html#buffer_new_buffer_string_encoding
  *    - https://nodejs.org/api/buffer.html#buffer_class_method_buffer_from_string_encoding
  */
-if (!Buffer.hasOwnProperty('from')) {
-  Buffer.prototype.from = Buffer.prototype.constructor
+function getBuffer(arg) {
+  if (Buffer.hasOwnProperty('from')){
+    return Buffer.from(arg)
+  }
+  return new Buffer(arg)
 }
-
 
 describe('Metalsmith', function(){
   beforeEach(function(){
@@ -251,7 +250,7 @@ describe('Metalsmith', function(){
         assert.deepEqual(files, {
           'index.md': {
             title: 'A Title',
-            contents: Buffer.from('body'),
+            contents: getBuffer('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -270,7 +269,7 @@ describe('Metalsmith', function(){
         assert.deepEqual(files, {
           'dir/index.md': {
             title: 'A Title',
-            contents: Buffer.from('body'),
+            contents: getBuffer('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -288,7 +287,7 @@ describe('Metalsmith', function(){
         assert.deepEqual(files, {
           'index.md': {
             title: 'A Title',
-            contents: Buffer.from('body'),
+            contents: getBuffer('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -304,7 +303,7 @@ describe('Metalsmith', function(){
         if (err) return done(err)
         assert.deepEqual(files, {
           'bin': {
-            contents: Buffer.from('echo test'),
+            contents: getBuffer('echo test'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -352,7 +351,7 @@ describe('Metalsmith', function(){
           'index.md': {
             date: new Date('2013-12-02'),
             title: 'A Title',
-            contents: Buffer.from('body'),
+            contents: getBuffer('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -374,7 +373,7 @@ describe('Metalsmith', function(){
           'index.md': {
             date: new Date('2013-12-02'),
             title: 'A Title',
-            contents: Buffer.from('body'),
+            contents: getBuffer('body'),
             mode: stats.mode.toString(8).slice(-4),
             stats: stats
           }
@@ -387,7 +386,7 @@ describe('Metalsmith', function(){
   describe('#write', function(){
     it('should write to a destination directory', function(done){
       var m = Metalsmith(fixture('write'))
-      var files = { 'index.md': { contents: Buffer.from('body') }}
+      var files = { 'index.md': { contents: getBuffer('body') }}
       m.write(files, function(err){
         if (err) return done(err)
         equal(fixture('write/build'), fixture('write/expected'))
@@ -397,7 +396,7 @@ describe('Metalsmith', function(){
 
     it('should write to a provided directory', function(done){
       var m = Metalsmith(fixture('write-dir'))
-      var files = { 'index.md': { contents: Buffer.from('body') }}
+      var files = { 'index.md': { contents: getBuffer('body') }}
       var dir = fixture('write-dir/out')
       m.write(files, dir, function(err){
         if (err) return done(err)
@@ -412,7 +411,7 @@ describe('Metalsmith', function(){
       var m = Metalsmith(fixture('write-mode'))
       var files = {
         'bin': {
-          contents: Buffer.from('echo test'),
+          contents: getBuffer('echo test'),
           mode: '0777'
         }
       }
@@ -508,7 +507,7 @@ describe('Metalsmith', function(){
         .use(function(files, metalsmith, done){
           Object.keys(files).forEach(function(file){
             var data = files[file]
-            data.contents = Buffer.from(data.title)
+            data.contents = getBuffer(data.title)
           })
           done()
         })
@@ -553,7 +552,7 @@ describe('Metalsmith', function(){
         .use(function(files, metalsmith, done){
           Object.keys(files).forEach(function(file){
             var data = files[file]
-            data.contents = Buffer.from(data.title)
+            data.contents = getBuffer(data.title)
           })
           done()
         })